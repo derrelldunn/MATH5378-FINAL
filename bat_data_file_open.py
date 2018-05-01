363a249972e8f416e1fd2fa35655873d37b7256a
Derrell Dunn <derrell.dunn@gmail.com>
2018-05-01 17:23:48 -0500

    Update file

--- bat_data_file_open.py
+++ bat_data_file_open.py
@@ -7,6 +7,7 @@ print(data.describe())
 trap_names = data["Trap"]
 print(trap_names)
 time_species = data[["Time", "Species"]]
+species_time = data[[ "Species", "Time"]]
 print(time_species)
 print(trap_names.describe())
 print(time_species.describe())
@@ -14,6 +15,8 @@ bat_weight=data[["Species", "Wt(g)"]]
 print(bat_weight)
 print(bat_weight.describe())
 bat_weight.boxplot()
+bat_weight.hist()
+#species_time.hist()
 plt.show()
 #add new code here
 
